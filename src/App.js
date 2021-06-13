2a78ba2c7eee7921a0020ee683a0a178ddef7e4a
saurabhtiwari7 <saurabhtiwari73841@gmail.com>
2021-06-13 10:45:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import bulbOff from './img/bulb-off.png';
+import bulbOn from './img/bulb-on.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+   this.state = {
+    open: true
+  }
 }
+  toggleImage = (e) => {
+   
+    this.setState(state => ({ open: !this.state.open }))
+  }
+
+  render() {
+    // const imageName = this.getImageName();
+    return (
+      <div>
+        <img style={{width: '20%'}} src={this.state.open ? bulbOn : bulbOff}  alt="toggle Images" /><br/>
+        <button style={{backgroundColor: 'red', padding: '2%', color: 'white'}} onClick={this.toggleImage}>Click</button>
+      </div>
+    );
+  }
+}
+// fvfvvv
 
 export default App;
